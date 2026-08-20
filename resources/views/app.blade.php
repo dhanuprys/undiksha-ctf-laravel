fcd53ecea1d4215a23f1931f86aa006fe1e43e7a
Dhanu Purnayasa <dhanuprys@gmail.com>
2026-08-21 07:15:30 +0800

    chore: add google analytics tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,16 @@
 <!-- SWIPER.. JANGAN MENCURI!!! -->
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NCFGSZPK5R"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-NCFGSZPK5R');
+    </script>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
